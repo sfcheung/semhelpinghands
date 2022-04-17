8b8427486dd4cac3a6a90446f9dbff7745375875
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-04-17 09:57:26 +0800

    Add a test of standardizedSolution_boot_ci for a CFA model

---
+++ tests/testthat/test-standardizedSolution_boot_ci_cfa.R
@@ -0,0 +1,52 @@
+skip_on_cran()
+skip_if(!interactive(),
+        message = "standardizedSolution_boot_ci not tested if not interactive")
+
+library(testthat)
+library(semhelpinghands)
+
+# Example from https://lavaan.ugent.be/tutorial/cfa.html
+options(width = 132)
+
+library(lavaan)
+data(HolzingerSwineford1939)
+model <-
+"
+visual  =~ x1 + x2 + x3
+textual =~ x4 + x5 + x6
+speed   =~ x7 + x8 + x9
+"
+
+set.seed(1234)
+system.time(fit <- cfa(model,
+                       data = HolzingerSwineford1939,
+                       se = "boot",
+                       bootstrap = 5000,
+                       parallel = "snow", ncpus = 8))
+
+ci_boot <- standardizedSolution_boot_ci(fit)
+
+get_std <- function(object) {
+    lavaan::standardizedSolution(object)$est.std
+  }
+fit2 <- update(fit, se = "none")
+set.seed(1234)
+boot_ci_test <- bootstrapLavaan(fit2, R = 5000,
+                                FUN = get_std,
+                                parallel = "snow",
+                                ncpus = 8)
+alpha <- .95
+ci_test <- t(apply(boot_ci_test, 2, quantile, probs = c((1 - alpha) / 2,
+                   1 - (1 - alpha) / 2),
+                   na.rm = TRUE))
+cbind(ci_test[, 1], ci_boot[, "boot.ci.lower"])
+cbind(ci_test[, 2], ci_boot[, "boot.ci.upper"])
+
+test_that("Compare boot ci", {
+    expect_equal(
+        ci_test,
+        as.matrix(ci_boot[, c("boot.ci.lower", "boot.ci.upper")]),
+        tolerance = .01,
+        ignore_attr = TRUE
+      )
+  })
