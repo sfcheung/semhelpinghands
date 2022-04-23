aaca0b56a0ec52bcdb5c8e915606f5d391d53cb3
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-04-23 12:15:40 +0800

    Add group_by_dvs
    Test passed.

---
+++ R/group_by_dvs.R
@@ -0,0 +1,146 @@
+#' @title Group Estimates By Dependent Variables
+#'
+#' @description It groups parameter estimates or other information such as
+#'               p-values into a table with dependent variables as columns
+#'               and independent variables as rows.
+#'
+#' @return A data frame.
+#'
+#' @param object A [lavaan-class] object, fitted with 'se = "boot"'.
+#'
+#' @param ... Optional arguments to be passed to
+#'            [lavaan::parameterEstimates()].
+#'
+#' @param col_name The column name of information to be
+#'                 grouped. Default is `"est"`. It accepts
+#'                 only one name.
+#'
+#' @param add_prefix If `TRUE`, the default,
+#'                   `colname` will be added as
+#'                   prefix to the column names of the
+#'                   output.
+#'
+#' @param group_first If `TRUE`, the rows will be grouped
+#'                    by groups first and then by
+#'                    independent variables Ignored if
+#'                    the model has only one group.
+#'                    Default is `FALSE`.
+#'
+#' @author Shu Fai Cheung (shufai.cheung@gmail.com)
+#'
+#' @examples
+#'
+#' library(lavaan)
+#' set.seed(5478374)
+#' n <- 50
+#' x <- runif(n) - .5
+#' m <- .40 * x + rnorm(n, 0, sqrt(1 - .40))
+#' y <- .30 * m + rnorm(n, 0, sqrt(1 - .30))
+#' dat <- data.frame(x = x, y = y, m = m)
+#' model <-
+#' '
+#' m ~ a*x
+#' y ~ b*m
+#' ab := a*b
+#' '
+#'
+#' fit <- sem(model, data = dat, fixed.x = FALSE)
+#' parameterEstimates(fit)
+#' group_by_dvs(fit)
+#'
+#' @export
+
+group_by_dvs <- function(lavaan_out,
+                         ...,
+                         col_name = "est",
+                         add_prefix = TRUE,
+                         group_first = FALSE) {
+    if (!inherits(lavaan_out, "lavaan")) {
+        stop("lavaan_out not a lavaan-class object.")
+      }
+    p_est <- lavaan::parameterEstimates(lavaan_out,
+                                        ...)
+    if (all(is.na(match(col_name, colnames(p_est))))) {
+        stop(paste(dQuote(col_name),
+              "not in the column names of the parameter estimate table."))
+      }
+    grouped <- lavaan::lavInspect(lavaan_out, "ngroups") > 1
+    dvs <- unique(p_est[p_est$op == "~", "lhs"])
+    p_est_list <- sapply(dvs, function(x, p_est) {
+                          out <- p_est[(p_est$lhs == x) &
+                                       (p_est$op == "~"), ]
+                        }, p_est = p_est, simplify = FALSE)
+    colname_list <- get_columns(col_name = col_name,
+                                p_est_list = p_est_list,
+                                grouped = grouped,
+                                group_first = group_first)
+    out <- merge_columns(colname_list,
+                         grouped = grouped,
+                         group_first = group_first)
+    if (!add_prefix) {
+        colnames(out) <- gsub(paste0(col_name, "_"), "",
+                              colnames(out))
+      }
+    out
+  }
+
+get_columns <- function(col_name,
+                        p_est_list,
+                        grouped = FALSE,
+                        group_first = FALSE) {
+    if (!grouped) {
+        m <- c("rhs")
+        m2 <- c("iv")
+      } else {
+        if (group_first) {
+            m <- c("rhs", "group")
+            m2 <- c("iv", "group")
+          } else {
+            m <- c("group", "rhs")
+            m2 <- c("group", "iv")
+          }
+      }
+    out <- mapply(function(x, x_name) {
+                      out <- x[, c(m, col_name)]
+                      colnames(out) <- c(m2, paste0(col_name, "_", x_name))
+                      out
+                    },
+                  p_est_list, names(p_est_list),
+                  SIMPLIFY = FALSE)
+    out
+  }
+# Merge all the list of columns
+`%merge%` <- function(x, y) {
+    merge(x,
+          y,
+          by = c("iv"),
+          all = TRUE)
+  }
+`%mergegp%` <- function(x, y) {
+    merge(x,
+          y,
+          by = c("iv", "group"),
+          all = TRUE)
+  }
+`%mergegp1st%` <- function(x, y) {
+    merge(x,
+          y,
+          by = c("group", "iv"),
+          all = TRUE)
+  }
+merge_columns <- function(col_list,
+                          grouped = FALSE,
+                          group_first = FALSE) {
+    if (grouped) {
+        if (group_first) {
+            out <- Reduce(`%mergegp1st%`, col_list)
+          } else {
+            out <- Reduce(`%mergegp%`, col_list)
+          }
+      } else {
+        out <- Reduce(`%merge%`, col_list)
+      }
+    #rownames(out) <- out$iv
+    #out <- out[, -1]
+    out
+  }
---
+++ tests/testthat/test-group_by_dvs.R
@@ -0,0 +1,56 @@
+# skip_on_cran()
+# skip_if(!interactive(),
+#         message = "standardizedSolution_boot_ci not tested if not interactive")
+
+library(testthat)
+library(semhelpinghands)
+
+n <- 100
+set.seed(4325)
+mod_pop <-
+"
+y1 ~ .2*x1 + .3*x2 + .1*x3
+y2 ~ .2*x1 + .2*x3
+y3 ~ .2*y2 + .2*x2
+"
+library(lavaan)
+dat <- simulateData(model = mod_pop, sample.nobs = n)
+mod <-
+"
+y1 ~ x1 + x2 + x3
+y2 ~ x1 + x3
+y3 ~ y2 + x2
+"
+fit <- sem(mod, dat)
+
+dat$gp <- sample(c("gp1", "gp2"), n, replace = TRUE)
+fit_gp <- sem(mod, dat, group = "gp")
+
+est_grouped <- group_by_dvs(fit)
+est_grouped_gp <- group_by_dvs(fit_gp)
+
+est_grouped_ans <- structure(list(iv = c("x1", "x2", "x3", "y2"), est_y1 = c(0.205762144483444, 
+0.380568486727359, 0.162192077222678, NA), est_y2 = c(0.149078624898469, 
+NA, 0.230428486323587, NA), est_y3 = c(NA, 0.295393228140568, 
+NA, 0.0443298135482514)), row.names = c(NA, -4L), class = "data.frame")
+
+est_grouped_gp_ans <- structure(list(iv = c("x1", "x1", "x2", "x2", "x3", "x3", "y2", 
+"y2"), group = c(1L, 2L, 1L, 2L, 1L, 2L, 1L, 2L), est_y1 = c(0.20954050563671, 
+0.258194633693387, 0.242853996647183, 0.483540060686917, 0.245745030252482, 
+0.120904062683077, NA, NA), est_y2 = c(0.0356595509573325, 0.225628736501381,
+NA, NA, 0.216741145006698, 0.260859641018626, NA, NA), est_y3 = c(NA, 
+NA, 0.372224774343152, 0.29070267615556, NA, NA, 0.0962380871786314,
+-0.00327088762324575)), row.names = c(NA, -8L), class = "data.frame")
+
+test_that("Test against know results", {
+    expect_equal(
+        est_grouped,
+        est_grouped_ans
+      )
+  })
+test_that("Test against know results", {
+    expect_equal(
+        est_grouped_gp,
+        est_grouped_gp_ans
+      )
+  })
