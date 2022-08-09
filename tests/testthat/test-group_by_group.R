e1f085cfe916d50341f5fb9fd1e48f97fd742448
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-08-09 23:48:35 +0800

    Add group_by_groups
    Tests and checks passed.

--- tests/testthat/test-group_by_group.R
+++ tests/testthat/test-group_by_group.R
@@ -1,4 +1,3 @@
-skip("WIP")
 # skip_on_cran()
 # skip_if(!interactive(),
 #         message = "standardizedSolution_boot_ci not tested if not interactive")
@@ -30,34 +29,38 @@ y2 ~ x3
 "
 fit2 <- sem(mod2, dat, group = "gp")
 
+mod3 <-
+"
+y1 ~ x1 + c(a1, a2)*x2 + c(b1, b2)*x3
+y2 ~ x3
+a1b1 := a1*b1
+"
+fit3 <- sem(mod3, dat, group = "gp")
+est3 <- parameterEstimates(fit3)
+
 est1 <- parameterEstimates(fit1)
-est1gp1 <- est1[est1$group == 1, ]
-est1gp2 <- est1[est1$group == 2, ]
+est1gp1 <- est1[est1$group == 2, ]
+est1gp2 <- est1[est1$group == 1, ]
 std1 <- standardizedSolution(fit1)
-std1gp1 <- std1[std1$group == 1, ]
-std1gp2 <- std1[std1$group == 2, ]
+std1gp1 <- std1[std1$group == 2, ]
+std1gp2 <- std1[std1$group == 1, ]
 
-out1 <- group_by_models(list(gpOne = est1gp1,
-                             gpTwo = est1gp2),
-                             col_names = c("est", "se"))
-#head(dplyr::filter(out1, op == "~"))
 
-out2 <- group_by_models(list(gpOne = std1gp1,
-                             gpTwo = std1gp2),
-                             standardized = TRUE,
-                             col_names = c("est.std", "se"))
-#head(dplyr::filter(out2, op == "~"))
+out1 <- group_by_groups(fit1, col_names = c("est", "se"))
+out1_chk <- group_by_models(list(gp1 = est1gp1,
+                                 gp2 = est1gp2),
+                            col_names = c("est", "se"))
+identical(out1, out1_chk[, c(1, 2, 3, 5, 4, 7, 6)])
 
-out3 <- group_by_models(list(model1 = fit1,
-                                  model2 = fit2),
-                             standardized = TRUE,
-                             col_names = c("std.all", "se"),
-                             group_first = TRUE)
-#head(dplyr::filter(out3, op == "~"))
-#tail(dplyr::filter(out3, op == "~"))
+out1std <- group_by_groups(std1, col_names = c("est.std", "se"),
+                           group_labels = lavInspect(fit1, "group.label"))
+out1std_chk <- group_by_models(list(gp1 = std1gp1,
+                                 gp2 = std1gp2),
+                            col_names = c("est.std", "se"))
+identical(out1std, out1std_chk[, c(1, 2, 3, 5, 4, 7, 6)])
 
-# test_that("Test against know results", {
-#     expect_equal(out1, out1_ans)
-#     expect_equal(out2, out2_ans)
-#     expect_equal(out3, out3_ans)
-#   })
+
+test_that("Test against know results", {
+    expect_equal(out1, out1_chk[, c(1, 2, 3, 5, 4, 7, 6)])
+    expect_equal(out1std, out1std_chk[, c(1, 2, 3, 5, 4, 7, 6)])
+  })
