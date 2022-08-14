453e69945a6f067e3140aab917c8d6b61cf876f4
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-08-14 22:45:58 +0800

    add_sig() supports `boot.ci`
    Tests and checks passed.

--- R/add_sig.R
+++ R/add_sig.R
@@ -5,17 +5,19 @@
 #'
 #' @details The function calls [lavaan::parameterEstimates()] or
 #'  [lavaan::standardizedSolution()] and check the columns
-#'  `pvalue` and/or `ci.lower` and `ci.upper` and
+#'  `pvalue`, `ci.lower` and `ci.upper`, and `boot.ci.lower`
+#'  and `boot.ci.upper` and
 #'  then insert columns to denote for each parameter estimate
 #'  whether it is significant.
-#'
 #'
 #' @return The output of [lavaan::parameterEstimates()] or
 #'  [lavaan::standardizedSolution()], with one or two columns
 #'  inserted after the parameter estimates to denote the
 #'  significant test results.
 #'
-#' @param object A [lavaan-class] object.
+#' @param object A [lavaan-class] object or the output
+#'               of [lavaan::parameterEstimates()] or
+#'               [lavaan::standardizedSolution()].
 #' @param ... Optional arguments to be passed to
 #'  [lavaan::parameterEstimates()] or
 #'  [lavaan::standardizedSolution()].
@@ -26,11 +28,16 @@
 #' @param na_str The string to be used for parameters
 #'  with no significant tests. For example, fixed
 #'  parameters. Default is `""`.
-#' @param use A character vector of one or two
-#'  elements. If one of them is `"pvalue"`,
-#'  *p*-values will be used. If one of them is
-#'  `"ci"`, confidence intervals will be used.
-#'  Can use both by `c("pvalue", "ci")`.
+#' @param use A character vector of one or more
+#'  elements. If `"pvalue"` is in the vector,
+#'  *p*-values will be used. If
+#'  `"ci"` is in the vector, confidence intervals will be used.
+#'  If `"boot.ci"` is in the vector and the columns
+#'  `boot.ci.lower` and `boot.ci.upper` are available,
+#'  these columns will be used.
+#'  Note that `ci.lower` and `ci.upper` can also
+#'  be bootstrap confidence intervals in some tables if
+#'  `se = "boot"` is used.
 #'
 #' @author Shu Fai Cheung <https://orcid.org/0000-0002-9871-9448>
 #'
@@ -64,7 +71,7 @@ add_sig <- function(object,
                     standardized = FALSE,
                     na_str = "",
                     use = "pvalue") {
-    use <- match.arg(use, choices = c("pvalue", "ci"), several.ok = TRUE)
+    use <- match.arg(use, choices = c("pvalue", "ci", "boot.ci"), several.ok = TRUE)
     args0 <- list(...)
     if (inherits(object, "lavaan")) {
         if (standardized) {
@@ -80,7 +87,7 @@ add_sig <- function(object,
             standardized <- FALSE
           }
       }
-    if ("pvalue" %in% use) {
+    if (("pvalue" %in% use) && !is.null(out$pvalue)) {
         pvalue <- out$pvalue
         psig <- stats::symnum(pvalue,
                             corr = FALSE,
@@ -90,37 +97,47 @@ add_sig <- function(object,
       } else {
         psig <- NULL
       }
-    if ("ci" %in% use) {
+    if (("ci" %in% use) && !is.null(out$ci.lower) &&
+                           !is.null(out$ci.upper)) {
         ci_chk <- ((out$ci.lower > 0) | (out$ci.upper < 0)) &
                   (out$ci.lower != out$ci.upper)
-        if ("level" %in% names(args0)) {
-            level = args0$level
-          } else {
-            level = .95
-          }
         csig <- ifelse(ci_chk, "Sig", "  ")
       } else {
         csig <- NULL
+      }
+    if (("boot.ci" %in% use) && !is.null(out$boot.ci.lower) &&
+                                !is.null(out$boot.ci.upper)) {
+        bci_chk <- ((out$boot.ci.lower > 0) | (out$boot.ci.upper < 0)) &
+                   (out$boot.ci.lower != out$boot.ci.upper)
+        bcsig <- ifelse(bci_chk, "Sig", "  ")
+      } else {
+        bcsig <- NULL
       }
     if (standardized) {
         k <- which(colnames(out) == "est.std")
       } else {
         k <- which(colnames(out) == "est")
       }
-    if (identical(use, "pvalue")) {
-        out0 <- data.frame(sig = format(psig))
-        k0 <- 1
-      } else if (identical(use, "ci")) {
-        out0 <- data.frame(ci.sig = format(csig))
-        k0 <- 1
-      } else {
-        out0 <- data.frame(sig = format(psig),
-                           ci.sig = format(csig))
-        k0 <- 2
+    out0 <- NULL
+    if ("pvalue" %in% use) {
+        out0 <- cbind(out0, sig = format(psig))
       }
-    out2 <- data.frame(out[c(1:k)],
-                        out0,
-                        out[c((k + k0):ncol(out))])
-    class(out2) <- class(out)
+    if ("ci" %in% use) {
+        out0 <- cbind(out0, ci = format(csig))
+      }
+    if ("boot.ci" %in% use) {
+        out0 <- cbind(out0, boot.ci = format(bcsig))
+      }
+    k0 <- ncol(out0)
+    k1 <- ncol(out)
+    if (!is.null(k0)) {
+        out2 <- out
+        tmp <- cbind(out0, out[, (k + 1):k1, drop = FALSE])
+        out2[, (k + 1):(k1 + k0)] <- tmp
+        colnames(out2)[(k + 1):(k1 + k0)] <- colnames(tmp)
+      } else {
+        out2 <- out
+      }
+    # class(out2) <- class(out)
     out2
   }
