7a6e347a8aa884700b317a6f9441f9a547690478
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-07-06 17:32:03 +0800

    WIP

---
+++ tests/testthat/test-plot_boot.R
@@ -0,0 +1,33 @@
+skip("WIP")
+library(testthat)
+library(semhelpinghands)
+
+# Example from https://lavaan.ugent.be/tutorial/mediation.html
+
+library(lavaan)
+set.seed(1234)
+n <- 1000
+X <- runif(n) - .5
+M <- 0.20*X + rnorm(n)
+Y <- 0.17*M + rnorm(n)
+Data <- data.frame(X = X, Y = Y, M = M)
+model <- ' # direct effect
+             Y ~ c*X
+           # mediator
+             M ~ a*X
+             Y ~ b*M
+           # indirect effect (a*b)
+             ab := a*b
+           # total effect
+             total := c + (a*b)
+         '
+set.seed(1234)
+# One bootstrap replication failed. Kept intentionally.
+suppressWarnings(system.time(fit <- sem(model,
+                       data = Data,
+                       se = "boot",
+                       bootstrap = 50)))
+
+fit_boot <- store_boot_def(fit)
+fit_boot <- store_boot_est_std(fit_boot)
+head(get_boot_def(fit_boot))
