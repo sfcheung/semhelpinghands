19853b30cb3c6736abb017d24b45638699041dce
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-09-17 19:39:07 +0800

    0.1.8.10: Update vignettes

--- rebuild_vignettes.R
+++ rebuild_vignettes.R
@@ -5,3 +5,7 @@ base_dir <- getwd()
 setwd("vignettes/")
 knitr::knit("standardizedSolution_boot_ci.Rmd.original", output = "standardizedSolution_boot_ci.Rmd")
 setwd(base_dir)
+
+# setwd("vignettes/articles/")
+# knitr::knit("plot_boot.Rmd.original", output = "plot_boot.Rmd")
+# setwd(base_dir)
